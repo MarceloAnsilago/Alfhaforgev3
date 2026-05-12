e2065388823f4e79579b8fa71d0bfc44f8b49aba
Marcelo Antonio <celosmg1@gmail.com>
2026-05-12 16:52:40 -0300

    take proft implementado

--- Frontend/views/dashboard_view.py
+++ Frontend/views/dashboard_view.py
@@ -3,6 +3,7 @@ import customtkinter as ctk
 from models.navigation import NavigationItem
 from themes.theme import UITheme
 from views.initial_settings_view import InitialSettingsView
+from views.take_profit_view import TakeProfitView
 from views.stop_movel_view import StopMovelView
 from views.stop_loss_view import StopLossView
 
@@ -77,6 +78,11 @@ class DashboardView(ctk.CTkFrame):
 
         if item.item_id == "stop_movel":
             self._current_body = StopMovelView(self._placeholder, self._theme)
+            self._current_body.grid(row=0, column=0, sticky="nsew", padx=14, pady=14)
+            return
+
+        if item.item_id == "take_profit":
+            self._current_body = TakeProfitView(self._placeholder, self._theme)
             self._current_body.grid(row=0, column=0, sticky="nsew", padx=14, pady=14)
             return
 
